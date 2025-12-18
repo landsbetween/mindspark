e546ac0318723d572c43fa8fc0ed173c2fb53cd1
shadowlycode <shadowlycode@gmail.com>
2025-12-18 20:56:22 +0200

    add icon for desing

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,18 @@
 import Link from 'next/link'
+import Image from 'next/image'
 
 export default function Header({ locale = 'ua' }) {
   return (
     <header className="navbar navbar-expand-lg navbar-light bg-light shadow-sm px-4">
-      <div className="container-fluid">
+      <div className="container-fluid d-flex align-items-center">
+        <Image
+          src="/logo.jpg"
+          alt="MindSpark Logo"
+          width={40}
+          height={40}
+          className="me-2 rounded"
+        />
+
         <Link href={`/${locale}`} className="navbar-brand text-primary fw-bold">
           MindSpark
         </Link>
