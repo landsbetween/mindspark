c0f73e8734d9aa8e48cd969e6ac147c847c27b89
shadowlycode <shadowlycode@gmail.com>
2026-01-19 16:23:35 +0200

    fix blog

--- app/[locale]/blog/page.jsx
+++ app/[locale]/blog/page.jsx
@@ -4,7 +4,7 @@ export default async function Blog({ params }) {
   const resolvedParams = await params; 
   const locale = resolvedParams?.locale === "en" ? "en" : "ua";
 
-  const url = `http://localhost:8080/api/public/articles?locale=${locale}`;
+  const url = `http://mindspark.dispatcher.space/api/public/articles?locale=${locale}`;
 
   const res = await fetch(url, {
     cache: "no-store",
